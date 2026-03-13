9b5d620fc465757f6b4a76bb03b181e389825a57
joshipurva12 <purva.joshi@bacancy.com>
2026-03-14 00:35:11 +0530

    init

---
+++ app/login/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useState } from "react"
+import { supabase } from "../../src/lib/supabaseClient"
+
+//git pls work
+export default function Login() {
+  const [email, setEmail] = useState("")
+
+  const login = async () => {
+    await supabase.auth.signInWithOtp({
+      email
+    })
+    alert("Check your email for login link")
+  }
+
+  return (
+    <div className="flex flex-col gap-4 p-10 max-w-sm">
+      <h1 className="text-2xl font-bold">Login</h1>
+
+      <input
+        className="border p-2"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <button
+        onClick={login}
+        className="bg-black text-white p-2 rounded"
+      >
+        Login
+      </button>
+    </div>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { supabase } from "@/lib/supabaseClient"
+
+export default function Home() {
+  const [tasks, setTasks] = useState<any[]>([])
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  async function fetchTasks() {
+    const { data } = await supabase
+      .from("tasks")
+      .select("*")
+
+    setTasks(data || [])
+  }
+
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold mb-6">
+        Team Task Board
+      </h1>
+
+      <div className="grid grid-cols-3 gap-6">
+
+        {tasks.map(task => (
+          <div
+            key={task.id}
+            className="border p-4 rounded-lg shadow"
+          >
+            <h2 className="font-semibold">
+              {task.title}
+            </h2>
+
+            <p className="text-sm text-gray-500">
+              {task.status}
+            </p>
+          </div>
+        ))}
+
+      </div>
+    </main>
+  )
+}
